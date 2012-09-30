d2c7d8202783674e9e40032ca33812803529083e
Jarmo Pertman <jarmo.p@gmail.com>
2012-09-30 19:02:11 +0300

    Add support for specifying container, setup block and browser.

--- lib/test/page.rb
+++ lib/test/page.rb
@@ -2,13 +2,46 @@ require File.expand_path("page/version", File.dirname(__FILE__))
 
 module Test
   class Page
-    attr_reader :container
+    extend Forwardable
 
-    def initialize(container)
-      @container = container
+    def_delegators :container, :present?, :p
+
+    class << self
+      attr_accessor :browser
+      attr_reader :setup_block, :container_block 
+
+      def container(&block)
+        @container_block = block
+      end
+
+      def setup(&block)
+        @setup_block = block
+      end
     end
 
-    def modify element, methodz
+    attr_writer :browser
+    attr_writer :container
+
+    def browser
+      @browser || parent_page_browser
+    end
+
+    def container
+      if @setup_block
+        instance_eval(&@setup_block) 
+        @setup_block = nil
+      end
+      @container ||= self.class.container_block && instance_eval(&self.class.container_block)
+    end
+
+    def initialize(container=nil, &block)
+      @container = container
+      @setup_block = self.class.setup_block
+
+      block.call self if block
+    end
+
+    def modify(element, methodz)
       methodz.each_pair do |meth, return_value|
         element.instance_eval do 
           singleton = class << self; self end
@@ -23,21 +56,28 @@ module Test
       element
     end
 
-    def redirect_to(page, container)
-      page.new container
+    def redirect_to(page, container=nil)
+      page.new container || self.container
     end      
 
     def method_missing(name, *args)
-      if @container.respond_to?(name)
-        self.class.class_eval %Q[
-          def #{name}(*args)
-            @container.send(:#{name}, *args) {yield}
-          end
-        ]
+      if container.respond_to?(name)
+        self.class.send :define_method, name do |*args|
+          container.send(name, *args) {yield}
+        end
         self.send(name, *args) {yield}
       else
         super
       end
+    end
+
+    private
+
+    def parent_page_browser
+      page_with_browser = self.class.ancestors.find do |klass|
+        klass.respond_to?(:browser) && klass.browser
+      end
+      page_with_browser ? page_with_browser.browser : nil
     end
       
   end
