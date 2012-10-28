6932d5482c875c4049e583ed600af4b8b0a651cd
Jarmo Pertman <jarmo.p@gmail.com>
2012-10-28 22:15:44 +0200

    Add YARD documentation.

--- lib/test/page.rb
+++ lib/test/page.rb
@@ -8,9 +8,30 @@ module Test
     def_delegators :element, :present?, :p
 
     class << self
+      # Set and get browser object for {Page}.
+      #
+      # This makes it possible to reuse one global browser object between
+      # different {Page} objects. It will be also shared between sub-classes of {Page}
+      # classes.
+      #
+      # @example Set Watir browser object as the browser
+      #   Test::Page.browser = Watir::Browser.new
       attr_accessor :browser
+
+      # @private
       attr_reader :element_block 
 
+      # Sets element for the {Page} via block.
+      # It will be evaluated lazily after {Page} has been instantiated.
+      #
+      # Element is like the container of the {Page} - everything outside of that element
+      # is not considered as part of the {Page}.
+      # Use as specific element as possible.
+      #
+      # @example Use Watir::Div as an {element}. 
+      #   class MyPage < Test::Page
+      #     element { browser.div(:id => "search") }
+      #   end
       def element(&block)
         @element_block = block
       end
@@ -18,10 +39,24 @@ module Test
 
     attr_writer :browser
 
+    # Set and get the browser object for specific {Page} instance.
+    #
+    # This is useful if some specific {Page} instance needs a different browser
+    # than is set via {.browser} method. Popup browser windows might be
+    # one example.
+    #
+    # If browser is not set via {#browser} then browser set via
+    # {.browser} will be used.
     def browser
       @browser || parent_page_browser
     end
 
+    # Gets the element instance.
+    #
+    # When {#setup} is defined, it will be executed once per {Page} instance.
+    #
+    # @return [Object] if element is specified for {#initialize}.
+    # @return [Object] otherwise {.element} block is evaluated once per {Page} instance.
     def element
       @setup_done ||= begin
                         setup if respond_to?(:setup)
@@ -33,12 +68,47 @@ module Test
                    end
     end
 
+    # @param [Object] Element element for the {Page}. {.element} will be used if not specified.
     def initialize(element=nil)
       @element = element
     end
 
-    def modify(element, methodz)
-      methodz.each_pair do |meth, return_value|
+    # Add or modify element methods.
+    #
+    # When element does not have the specified method then that method will be
+    # added to the specific element instance.
+    # When element has specified method it will be invoked before invoking the
+    # specified method.
+    #
+    # @example Add #png? method to Watir::Image
+    #   class Gallery < Test::Page
+    #     def thumbnail
+    #       image = img(:id => "thumbnail")
+    #       modify image,
+    #         :png? => proc { File.extname(image.src).downcase == ".png" }
+    #     end
+    #   end
+    #
+    #   Gallery.new.thumbnail.png? # returns true for images, which have
+    #                              # the src attribute set to png.
+    #
+    # @example Modify Watir::Button#click to return new MainPage instance after click
+    #   class LoginForm < Test::Page
+    #     def login_button
+    #       modify button(:id => "login"),
+    #         :click => proc { redirect_to MainPage }
+    #     end
+    #   end
+    #
+    #   LoginForm.new.login_button.click # performs the click and returns
+    #                                    # a new MainPage instance.
+    #
+    # @param [Object] Element element to modify.
+    # @param [Hash<Symbol,Proc>] Hash of method name as a Symbol and body as a Proc pairs.
+    #
+    # @return [Object] Modified Element instance.
+    def modify(element, methods)
+      methods.each_pair do |meth, return_value|
         element.instance_eval do 
           singleton = class << self; self end
 
@@ -52,10 +122,15 @@ module Test
       element
     end
 
+    # Convenience method for creating new {Page} object.
+    #
+    # @param [Page] Page page class to make an instance of
+    # @param [Object] Element optional element instance. When not specified current {Page} element will be used.
     def redirect_to(page, element=nil)
       page.new element || self.element
     end      
 
+    # Proxies every method call not found on {Page} to element.
     def method_missing(name, *args)
       if element.respond_to?(name)
         self.class.send :define_method, name do |*args|
--- test-page.gemspec
+++ test-page.gemspec
@@ -4,9 +4,9 @@ require File.expand_path('../lib/test/page/version', __FILE__)
 Gem::Specification.new do |gem|
   gem.authors       = ["Jarmo Pertman"]
   gem.email         = ["jarmo.p@gmail.com"]
-  gem.description   = %q{TODO: Write a gem description}
-  gem.summary       = %q{TODO: Write a gem summary}
-  gem.homepage      = ""
+  gem.description   = %q{Test::Page helps you to write easily maintainable integration tests by using Watir, Selenium or any other testing library.}
+  gem.summary       = %q{Test::Page helps you to write easily maintainable integration tests by using Watir, Selenium or any other testing library.}
+  gem.homepage      = "https://github.com/jarmo/test-page"
 
   gem.files         = `git ls-files`.split($\)
   gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
@@ -17,4 +17,6 @@ Gem::Specification.new do |gem|
 
   gem.add_development_dependency "rspec", "~>2.0"
   gem.add_development_dependency "simplecov"
+  gem.add_development_dependency "yard"
+  gem.add_development_dependency "redcarpet"
 end
